0dd14ef9934f6c196dfe147d24d1ceceb9e3e07d
Borja Yanini <byanini@gmail.com>
2020-07-16 14:18:16 +0200

    created todo-apps project. Imported npm todomvc-app-css library

---
+++ 03-todoapp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TodoModule } from './todos/todo.module';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    TodoModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
