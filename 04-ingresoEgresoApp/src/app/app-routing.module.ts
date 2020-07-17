8c349eb8502a51d54e9c0d6c9f578097c519be05
Borja Yanini <byanini@gmail.com>
2020-07-17 16:05:45 +0200

    create new children routes. Added ingreso, dashboard and detalle templates

--- 04-ingresoEgresoApp/src/app/app-routing.module.ts
+++ 04-ingresoEgresoApp/src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { dashboardRouter } from './dashboard/dashboard.routes';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: '',
+    component: DashboardComponent,
+    children: dashboardRouter
+  },
+  {
+    path: '**', redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
