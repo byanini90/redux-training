77444947379cda4248282a009c54cfbd4974bd10
Borja Yanini <byanini@gmail.com>
2020-07-20 19:06:49 +0200

    imported sweetalert2 and firestore

--- 04-ingresoEgresoApp/src/environments/environment.prod.ts
+++ 04-ingresoEgresoApp/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD0v1iNNqVk2z0f5-9gcG3SXwYYfsfVKnQ',
+    authDomain: 'gotyapp-d012a.firebaseapp.com',
+    databaseURL: 'https://gotyapp-d012a.firebaseio.com',
+    projectId: 'gotyapp-d012a',
+    storageBucket: 'gotyapp-d012a.appspot.com',
+    messagingSenderId: '544955995848',
+    appId: '1:544955995848:web:a7ce1da11dab2a70e7b7fd'
+  }
 };
--- 04-ingresoEgresoApp/src/environments/environment.ts
+++ 04-ingresoEgresoApp/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD0v1iNNqVk2z0f5-9gcG3SXwYYfsfVKnQ',
+    authDomain: 'gotyapp-d012a.firebaseapp.com',
+    databaseURL: 'https://gotyapp-d012a.firebaseio.com',
+    projectId: 'gotyapp-d012a',
+    storageBucket: 'gotyapp-d012a.appspot.com',
+    messagingSenderId: '544955995848',
+    appId: '1:544955995848:web:a7ce1da11dab2a70e7b7fd'
+  }
 };
 
 /*
