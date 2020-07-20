072eb1db58699789cd0126bfe9cf5e24a3a5c012
Borja Yanini <byanini@gmail.com>
2020-07-20 19:09:55 +0200

    used auth firestore

--- 04-ingresoEgresoApp/src/app/auth/login/login.component.ts
+++ 04-ingresoEgresoApp/src/app/auth/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +13,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  login() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+    Swal.fire({
+      title: 'Loading...',
+      onBeforeOpen: () => {
+        Swal.showLoading();
+      }
+    });
+
+    const {correo, password} = this.loginForm.value;
+    this.authService.loginUsuario(correo, password)
+      .then(credenciales => {
+        this.router.navigate(['/']);
+      })
+      .catch(error => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: error.message,
+        });
+      })
+      .finally( () => Swal.close());;
   }
 
 }
