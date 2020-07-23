65f6c8b24a2047a5e3a4ee54efad772242c7df82
Borja Yanini <byanini@gmail.com>
2020-07-23 11:11:48 +0200

    created shared module

--- 04-ingresoEgresoApp/src/app/app.module.ts
+++ 04-ingresoEgresoApp/src/app/app.module.ts
@@ -21,13 +21,11 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso.component';
 import { EstadisticaComponent } from './ingreso-egreso/estadistica/estadistica.component';
 import { DetalleComponent } from './ingreso-egreso/detalle/detalle.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { OrdenIngresoPipe } from './pipes/orden-ingreso.pipe';
 
 // Modulos
 import { AuthModule } from './auth/auth.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -36,14 +34,12 @@ import { AuthModule } from './auth/auth.module';
     IngresoEgresoComponent,
     EstadisticaComponent,
     DetalleComponent,
-    FooterComponent,
-    NavbarComponent,
-    SidebarComponent,
     OrdenIngresoPipe
   ],
   imports: [
     BrowserModule,
     AuthModule,
+    SharedModule,
     ChartsModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
